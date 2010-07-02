35582f6c333ead125455dcbf73da479110b6224d
Craig Campbell <iamcraigcampbell@gmail.com>
2010-07-01 20:51:39 -0400

    adding a verbose mode for output displaying what the script is actually showing you fixing a bug where all revisions have already been merged adding target url to the mergeinfo call so the script can run from a subdirectory

--- show_missing_revs.sh
+++ show_missing_revs.sh
@@ -26,6 +26,7 @@ usage_and_exit()
     echo "-b branch => name of branch that you want to merge to current branch\n"
     echo "optional arguments:"
     echo "-u username => target svn user to see what he/she has not yet merged"
+    echo "-v verbose => show more information about what is happening as it happens"
     echo "-h help => show these instructions"
     exit 1
 }
@@ -33,6 +34,7 @@ usage_and_exit()
 repo=
 branch=
 username=
+verbose=
 
 # magically determine the repo url
 repo_url=`svn info | grep "URL:" | cut -d ' ' -f 2`
@@ -40,23 +42,30 @@ branch_url=`echo $repo_url | grep "/branches"`
 
 # are we in a branch?
 if [ $branch_url ] ; then
+    merge_to=`echo $branch_url | sed s:/branches/:' ':g | cut -d ' ' -f 2 | cut -d '/' -f 1`
     repo=`echo $branch_url | sed s:/branches:' ':g | cut -d ' ' -f 1`
+    target=$repo/branches/$merge_to
 else
+    merge_to="trunk"
     repo=`echo $repo_url | sed s:/trunk:' ':g | cut -d ' ' -f 1`
+    target=$repo/$merge_to
 fi
 
 # gather options
-while getopts 'hu:b:' OPTION
+while getopts 'vhu:b:' OPTION
 do
     case $OPTION in
-    b)    if [ $OPTARG == "trunk" ]
-          then
+    b)    if [ $OPTARG == "trunk" ] ; then
+              merge_from="trunk"
               branch="$repo/trunk"
           else
+              merge_from=$OPTARG
               branch="$repo/branches/$OPTARG"
           fi
           ;;
     u)    username="$OPTARG"
+          ;;
+    v)    verbose=true
           ;;
     h)    usage_and_exit
           ;;
@@ -67,12 +76,46 @@ if [ ! $branch ] ; then
     usage_and_exit
 fi
 
+# craft a nice sentence in english of what we are doing
+message="displaying revisions in ${merge_from}"
+
+if [ ! $branch_url ] ; then
+    message=$message" branch"
+fi
+
+if [ $username ] ; then
+    message=$message" by ${username}"
+fi
+
+message=$message" that have not yet been merged to ${merge_to}"
+
+if [ $branch_url ] ; then
+    message=$message" branch"
+fi
+
+# tell the user what we are doing
+echo $message
+
+if [ ! $verbose ] ; then
+    echo ""
+fi
+
 # put all revisions that have not been merged into a text file
-svn mergeinfo --show-revs eligible $branch > eligible_revs.txt
+if [ $verbose ] ; then
+    echo "getting missing revisions from svn..."
+fi
+
+svn mergeinfo --show-revs eligible $branch $target > eligible_revs.txt
 
 # turn these revisions into an array
 revision_list=`cat eligible_revs.txt`
 revisions=(`echo $revision_list | tr '\n' ' '`) 
+
+# if everything has already been merged then let's stop
+if [ ${#revisions[*]} == "0" ] ; then
+    echo "everything from ${merge_from} has been merged to ${merge_to}"
+    exit 0
+fi
 
 # get the count of the last item
 # (-1 because there is a new line at end of the file)
@@ -83,7 +126,15 @@ first_revision=${revisions[0]}
 final_revision=${revisions[$final_count]}
 
 # get the svn log for this range and write it out to a file for now
-svn log $branch -$first_revision:$final_revision > revisions_in_range.txt
+if [ $verbose ] ; then
+    echo getting the log from svn"..."
+fi
+
+svn log -$first_revision:$final_revision $branch > revisions_in_range.txt
+
+if [ $verbose ] ; then
+    echo "processing results...\n"
+fi
 
 # find the intersection between the missing revs and the logs
 output=
